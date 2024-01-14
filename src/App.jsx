18709beaf125ac89c823f5a70987e38a9a843ea0
Harsh Suvarna <harsh.suvarna0@gmail.com>
2024-01-14 01:38:37 +0000

    created landing page structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="landing-page">asdfasdf</div>
+      <div className="landing-page">sdfasfasdfi</div>
+    </>
+  );
+}
+
+export default App;
